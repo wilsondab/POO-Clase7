04b50f76ceb6f58e44b443ef3ca52a7db1ac7c79
Alejandro Jara <alejandroejara@hotmail.com>
2024-06-04 12:17:44 -0300

    Ejercicio4Modulo2

---
+++ Ejercicio4Modulo2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Ejercicio4Modulo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string path = $"{AppDomain.CurrentDomain.BaseDirectory}\\data.txt";
+
+
+
+        }
+      
+    }
+}
